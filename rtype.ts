f8463c2c8993e2bbe4724c3428bc01867cf5aa45
4t145 <u4t145@163.com>
2025-02-24 14:28:07 +0800

    update

--- rtype.ts
+++ rtype.ts
@@ -1,5 +1,5 @@
 type IntBitSize = 8 | 16 | 32 | 64 | 128
-type FloatBitSize = 16 | 32 | 64
+type FloatBitSize = 16 | 32 | 64 | 128
 export type PrimitiveTypeMarker = `u${IntBitSize}` | `i${IntBitSize}` | `f${FloatBitSize}` | 'bool' | 'String'
 
 // layout: 1. primitive 2. sum type, 3. product type, 4. collection type
@@ -36,7 +36,7 @@ export type View<L extends Layout> = {
 
 
 export type UnitTypeMarker = 'unit'
-export type RustType = TypeKindMarker<UnitTypeMarker> | TypeKindMarker<PrimitiveTypeMarker> | TupleType | ArrayType | StructType | EnumType | CollectionType;
+export type Type = TypeKindMarker<UnitTypeMarker> | TypeKindMarker<PrimitiveTypeMarker> | TupleType | ArrayType | StructType | EnumType | CollectionType;
 export const TYPE_KIND: unique symbol = Symbol('type-kind')
 export type TypeKind = UnitTypeMarker | PrimitiveTypeMarker | 'struct' | 'enum' | 'tuple' | 'array' | 'collection'
 export type TypeKindMarker<T extends TypeKind> = {
@@ -83,16 +83,18 @@ export type f32 = TypeKindMarker<'f32'>;
 export const f32: f32 = TypeKindMarker('f32');
 export type f64 = TypeKindMarker<'f64'>;
 export const f64: f64 = TypeKindMarker('f64');
+export type f128 = TypeKindMarker<'f128'>;
+export const f128 = TypeKindMarker('f128');
 export type bool = TypeKindMarker<'bool'>;
 export const bool: bool = TypeKindMarker('bool');
 export type String = TypeKindMarker<'String'>;
 export const String: String = TypeKindMarker('String');
 
 
-export type TupleType = RustType[] & TypeKindMarker<'tuple'>
+export type TupleType = Type[] & TypeKindMarker<'tuple'>
 
 export type StructType = {
-    [field: string]: RustType
+    [field: string]: Type
 } & TypeKindMarker<'struct'>
 
 export type EnumVariantType = (Unit | TupleType | StructType)
@@ -121,40 +123,41 @@ export type EnumType = {
     [variant: string]: EnumVariant
 } & TypeKindMarker<'enum'>
 
+
 export type ArrayType = {
-    element: RustType,
+    element: Type,
     size: number
 } & TypeKindMarker<'array'>
 
 export type CollectionType = {
-    element: RustType,
+    element: Type,
 } & TypeKindMarker<'collection'>
 
-export type Collection<T extends RustType> = {
+export type Collection<T extends Type> = {
     element: T,
 } & TypeKindMarker<'collection'>
 
-export const Collection = <T extends RustType>(element: T): Collection<T> => (
+export const Collection = <T extends Type>(element: T): Collection<T> => (
     {
         element,
         ...TypeKindMarker('collection')
     }
 )
 
-export type Array<T extends RustType, N extends number> = {
+export type Array<T extends Type, N extends number> = {
     element: T
     size: N
 } & TypeKindMarker<'array'>
 
-export const Array = <T extends RustType, N extends number>(element: T, size: N): Array<T, N> => ({
+export const Array = <T extends Type, N extends number>(element: T, size: N): Array<T, N> => ({
     element,
     size,
     ...TypeKindMarker('array')
 })
 
-export type Tuple<T extends RustType[]> = T & TypeKindMarker<'tuple'>
+export type Tuple<T extends Type[]> = T & TypeKindMarker<'tuple'>
 
-export const Tuple = <T extends RustType[]>(...element: T): Tuple<T> => {
+export const Tuple = <T extends Type[]>(...element: T): Tuple<T> => {
     const elementAsTuple = element as Tuple<T>;
     elementAsTuple[TYPE_KIND] = 'tuple'
     return elementAsTuple
@@ -173,40 +176,40 @@ export const Enum = <T extends {
 }
 
 export type Struct<T extends {
-    [variant: string]: RustType
+    [variant: string]: Type
 }> = T & TypeKindMarker<'struct'>
 
 export const Struct = <T extends {
-    [variant: string]: RustType
+    [variant: string]: Type
 }>(fields: T): Struct<T> => {
     const elementAsStruct = fields as Struct<T>;
     elementAsStruct[TYPE_KIND] = 'struct'
     return elementAsStruct
 }
 
-export type Result<T extends RustType, E extends RustType> = Enum<{
+export type Result<T extends Type, E extends Type> = Enum<{
     Ok: EnumVariant<0, Tuple<[T]>>,
     Err: EnumVariant<1, Tuple<[E]>>,
 }>
 
-export const Result = <T extends RustType, E extends RustType>(ok: T, err: E): Result<T, E> => Enum({
+export const Result = <T extends Type, E extends Type>(ok: T, err: E): Result<T, E> => Enum({
     Ok: _(0, Tuple(ok)),
     Err: _(1, Tuple(err))
 })
 
-export type Option<T extends RustType> = Enum<{
+export type Option<T extends Type> = Enum<{
     Some: EnumVariant<0, Tuple<[T]>>,
     None: EnumVariant<1, Unit>,
 }>
 
-export const Option = <T extends RustType>(value: T): Option<T> => Enum({
+export const Option = <T extends Type>(value: T): Option<T> => Enum({
     Some: _(0, Tuple(value)),
     None: _(1)
 })
 
 export const Vec = Collection
 export const Set = Collection
-export const Map = <K extends RustType, V extends RustType>(K: K, V: V): Collection<Tuple<[K, V]>> => (Collection(Tuple(K, V)))
+export const Map = <K extends Type, V extends Type>(K: K, V: V): Collection<Tuple<[K, V]>> => (Collection(Tuple(K, V)))
 export const Bytes = Collection(u8)
 
 
@@ -214,7 +217,7 @@ export const Bytes = Collection(u8)
     RustType To Layout
 */
 
-type RustLayout<T extends RustType> =
+type RustLayout<T extends Type> =
     T extends Unit ? UnitLayout :
     T extends TypeKindMarker<infer P extends PrimitiveTypeMarker> ? LayoutMarker<'primitive'> & P :
     T extends StructType ? ProductLayout :
@@ -226,15 +229,15 @@ type RustLayout<T extends RustType> =
 
 
 
-type TupleValue<T extends RustType[]> =
+type TupleValue<T extends Type[]> =
     T extends [infer First, ...infer Tail] ?
-    First extends RustType ?
-    Tail extends RustType[] ?
+    First extends Type ?
+    Tail extends Type[] ?
     [Value<First>, ...TupleValue<Tail>] :
     never :
     never :
     T extends [infer First] ?
-    First extends RustType ?
+    First extends Type ?
     [Value<First>] :
     never :
     never
@@ -257,14 +260,17 @@ export type JsonEnumVariant<K> = {
 }
 export type EnumVariantValue<K, V> = V & JsonEnumVariant<K>
 export const EnumVariantValue = <K, V>(variant: K, value: V): EnumVariantValue<K, V> => {
-    let asJsonEnumVariant = value as EnumVariantValue<K, V>;
+    let asJsonEnumVariant = {
+        [VARIANT]: variant,
+        ...value
+    } as EnumVariantValue<K, V>;
     asJsonEnumVariant[VARIANT] = variant;
     return asJsonEnumVariant
 }
 export const $ = EnumVariantValue;
-export type Value<T extends RustType> =
+export type Value<T extends Type> =
     T extends Unit ? {} :
-    T extends TypeKindMarker<`${'u' | 'i'}${8 | 16 | 32}` | `f${16 | 32 | 64}`> ? number :
+    T extends TypeKindMarker<`${'u' | 'i'}${8 | 16 | 32}` | `f${16 | 32 | 64 | 128}`> ? number :
     T extends TypeKindMarker<`${'u' | 'i'}${64}`> ? bigint :
     T extends TypeKindMarker<'bool'> ? boolean :
     T extends TypeKindMarker<'String'> ? string :
@@ -273,7 +279,7 @@ export type Value<T extends RustType> =
     } :
     T extends TupleType ?
     T extends [infer First] ?
-    First extends RustType ?
+    First extends Type ?
     Value<First> :
     never :
     TupleValue<T> :
@@ -284,6 +290,328 @@ export type Value<T extends RustType> =
 
 
 const ResultInJson: Value<Result<u32, String>> = $("Ok", 10);
+
+
+
+
+
+type BincodeConfig = {
+    endian: 'big' | 'little',
+    int_encoding: 'variant' | 'fixed',
+    limit?: number
+}
+const STANDARD: BincodeConfig = {
+    endian: 'little',
+    int_encoding: 'fixed',
+}
+export const decode = <T extends Type>(definition: T, bytes: ArrayBuffer, config: BincodeConfig = STANDARD): {
+    value: Value<T>
+    offset: number
+} => {
+    let view = new DataView(bytes);
+    const littleEndian = config.endian === 'little';
+    let value: Value<T> = {} as Value<T>;
+    let offset = 0;
+    switch (definition[TYPE_KIND]) {
+        case "unit":
+            value = {} as Value<T>
+        case "u8":
+            value = view.getUint8(offset) as Value<T>
+            offset += 1;
+            break
+        case "u16":
+            value = view.getUint16(offset, littleEndian) as Value<T>
+            offset += 2;
+            break
+        case "u32":
+            value = view.getUint32(offset, littleEndian) as Value<T>
+            offset += 4;
+            break
+        case "u64":
+            value = view.getBigUint64(offset, littleEndian) as Value<T>
+            offset += 8;
+            break
+        case "u128":
+            throw ("not supported yet")
+        case "i8":
+            value = view.getInt8(offset) as Value<T>
+            offset += 1;
+            break
+        case "i16":
+            value = view.getInt16(offset, littleEndian) as Value<T>
+            offset += 2;
+            break
+        case "i32":
+            value = view.getInt32(offset, littleEndian) as Value<T>
+            offset += 4;
+            break
+        case "i64":
+            value = view.getBigInt64(offset, littleEndian) as Value<T>
+            offset += 8;
+            break
+        case "i128":
+            throw ("not supported yet")
+        case "f16":
+            throw ("not supported yet")
+        case "f32":
+            value = view.getFloat32(offset, littleEndian) as Value<T>
+            offset += 4;
+            break
+        case "f64":
+            value = view.getFloat64(offset, littleEndian) as Value<T>
+            offset += 8;
+            break
+        case "bool":
+            value = (view.getUint8(offset) === 0) as Value<T>
+            offset += 1;
+            break
+        case "String":
+            {
+                const byteLength = Number(view.getBigUint64(offset, littleEndian));
+                const decoder = new TextDecoder();
+                value = decoder.decode(new Uint8Array(bytes, 8, Number(byteLength))) as Value<T>
+                offset += byteLength;
+            }
+            break
+        case "collection":
+            {
+                const byteLength = Number(view.getBigUint64(0, littleEndian));
+                offset += 8;
+                const elementDefinition = definition['element'];
+                const collection = [] as Value<Type>[];
+                for (let index = 0; index < byteLength; index += 1) {
+                    const {
+                        value: element,
+                        offset: elementOffset
+                    } = decode(elementDefinition, bytes.slice(offset), config);
+                    offset += elementOffset
+                    collection.push(element)
+                }
+                value = collection as Value<T>
+            }
+        case "tuple":
+            {
+                const tupleDefinition = definition as TupleType;
+                const tupleValue = [] as Value<Type>[];
+                for (const elementDefinition of tupleDefinition) {
+                    const {
+                        value: element,
+                        offset: elementOffset
+                    } = decode(elementDefinition, bytes.slice(offset), config);
+                    offset += elementOffset
+                    tupleValue.push(element)
+                }
+                value = tupleValue as Value<T>
+            }
+            break
+        case "array":
+            {
+                const arrayDefinition = definition as ArrayType;
+                const tupleValue = [] as Value<Type>[];
+                const elementDefinition = arrayDefinition.element;
+                for (let index = 0; index < arrayDefinition.size; index += 1) {
+                    const {
+                        value: element,
+                        offset: elementOffset
+                    } = decode(elementDefinition, bytes.slice(offset), config);
+                    offset += elementOffset
+                    tupleValue.push(element)
+                }
+                value = tupleValue as Value<T>
+            }
+            break
+        case "struct":
+            {
+                const structDefinition = definition as StructType;
+                let decodedObject = {} as any;
+                for (const field in Object.keys(definition)) {
+                    const type = structDefinition[field];
+                    const {
+                        value: fieldValue,
+                        offset: fieldOffset
+                    } = decode(type, bytes.slice(offset), config);
+                    decodedObject[field] = fieldValue;
+                    offset += fieldOffset;
+                }
+                value = decodedObject as Value<T>
+            }
+            break
+        case "enum":
+            {
+                function indexed(e: EnumType): {
+                    [index: number]: [Type, string]
+                } {
+                    const indexedDefinition = {}
+                    for (const variant in e) {
+                        if (typeof variant === 'string') {
+                            const variantType = e[variant]
+                            indexedDefinition[variantType[SYMBOL_EXPR]] = [variantType, variant]
+                        }
+                    }
+                    return indexedDefinition
+                }
+                const enumDefinition = definition as EnumType;
+                const indexedDefinition = indexed(enumDefinition)
+                const variantIndex = view.getUint32(offset);
+                offset += 4;
+                const [variantType, variant] = indexedDefinition[variantIndex];
+                const { value: variantValue, offset: variantOffset } = decode(variantType, bytes.slice(offset), config);
+                offset += variantOffset;
+                value = EnumVariantValue(variant, variantValue) as Value<T>
+            }
+            break
+    }
+    return {
+        value,
+        offset,
+    }
+}
+
+
+export const encode = <T extends Type>(type: T, value: Value<T>, buffer: ArrayBuffer, config: BincodeConfig = STANDARD): number => {
+    let dataView = new DataView(buffer);
+    let offset = 0
+    const littleEndian = config.endian === 'little';
+
+    switch (type[TYPE_KIND]) {
+        case "unit": {
+            break
+        }
+        case "u8": {
+            dataView.setUint8(offset, value as number);
+            offset += 1;
+            break
+        }
+        case "u16": {
+            dataView.setUint16(offset, value as number, littleEndian);
+            offset += 2;
+            break
+        }
+        case "u32": {
+            dataView.setUint32(offset, value as number, littleEndian);
+            offset += 4;
+            break
+        }
+        case "u64": {
+            dataView.setBigUint64(offset, value as bigint, littleEndian);
+            offset += 8;
+            break
+        }
+        case "u128": {
+            throw ("unimplemented")
+        }
+        case "i8": {
+            dataView.setInt8(offset, value as number);
+            offset += 1;
+            break
+        }
+        case "i16": {
+            dataView.setInt16(offset, value as number, littleEndian);
+            offset += 2;
+            break
+        }
+        case "i32": {
+            dataView.setInt32(offset, value as number, littleEndian);
+            offset += 4;
+            break
+        }
+        case "i64": {
+            dataView.setBigInt64(offset, value as bigint, littleEndian);
+            offset += 8;
+            break
+        }
+        case "i128": {
+            throw ("unimplemented")
+        }
+        case "f16": {
+            throw ("unimplemented")
+        }
+        case "f32": {
+            dataView.setFloat32(offset, value as number, littleEndian);
+            offset += 4;
+            break
+        }
+        case "f64": {
+            dataView.setFloat64(offset, value as number, littleEndian);
+            offset += 8;
+            break
+        }
+        case "f128": {
+            throw ("unimplemented")
+        }
+        case "bool": {
+            dataView.setUint8(offset, value as number);
+            offset += 1;
+            break
+        }
+        case "String": {
+            const encoder = new TextEncoder();
+            const encoded = encoder.encode(value as string);
+            dataView.setBigUint64(offset, BigInt(encoded.byteLength), littleEndian);
+            offset += 8;
+            new Uint8Array(buffer).set(encoded, offset);
+            offset += encoded.byteLength;
+            break
+        }
+        case "tuple": {
+            const tupleType = type as TupleType;
+            const tupleValue = value as TupleValue<typeof tupleType>;
+            if (tupleType.length === 1) {
+                offset += encode(tupleType[0], tupleValue[0], buffer.slice(offset), config)
+            } else {
+                for (let index = 0; index < tupleType.length; index += 1) {
+                    offset += encode(tupleType[index], tupleValue[index], buffer.slice(offset), config)
+                }
+            }
+            break
+        }
+        case "array": {
+            const arrayType = type as ArrayType;
+            const arrayValue = value as Value<typeof arrayType>;
+            for (let index = 0; index < arrayType.size; index += 1) {
+                offset += encode(arrayType.element, arrayValue[index], buffer.slice(offset), config)
+            }
+            break
+        }
+        case "struct": {
+            const structType = type as StructType;
+            const structValue = value as Value<typeof structType>;
+            for (const field in structType) {
+                offset += encode(structType[field], structValue[field], buffer.slice(offset), config)
+            }
+            break
+        }
+        case "enum": {
+            const enumType = type as EnumType;
+            const enumValue = value as EnumValue<typeof enumType>;
+            const variantIndex = enumType[enumValue[VARIANT]][SYMBOL_EXPR];
+            dataView.setUint32(offset, variantIndex, littleEndian);
+            offset += 4;
+            const variant = enumType[enumValue[VARIANT]];
+            offset += encode(variant, enumValue, buffer.slice(offset), config)
+            break
+        }
+        case "collection": {
+            const collectionType = type as CollectionType;
+            const collectionValue = value as Value<typeof collectionType>;
+            dataView.setBigUint64(offset, BigInt(collectionValue.length), littleEndian);
+            offset += 8;
+            for (const element of collectionValue) {
+                offset += encode(collectionType.element, element, buffer.slice(offset), config)
+            }
+            break
+        }
+    }
+    return offset
+}
+
+let buffer = new ArrayBuffer(1024);
+console.log($("Ok", 5));
+const encoded = encode<Result<u8, String>>(Result(u8, String), $("Ok", 5), buffer);
+console.log(encoded);
+const decoded = decode(Result(u8, String), buffer).value;
+console.log(decoded);
+
 // export type StructValue<T extends StructType> = {
 //     [K in Exclude<keyof T, typeof SYMBOL_LAYOUT>]: T[K] extends RustType ? RustValue<T[K]> : never
 // } & SlicePart
